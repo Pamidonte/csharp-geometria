903d3257afc74726f5fdb0383c545a019999d84b
Pamidonte <francescomarrone8@gmail.com>
2022-11-26 17:19:41 +0100

    finito ma in ritardo

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Geometria
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int baseRettangolo = 0;
+                int altezzaRettangolo = 0;
+                string nomeRettangolo = "";
+                Console.WriteLine("inserisci il nome del rettangolo: ");
+                nomeRettangolo = Console.ReadLine();
+                Console.WriteLine("inserisci la base del tuo rettangolo: ");
+                baseRettangolo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\ninserisci l altezza del tuo rettangolo: ");
+                altezzaRettangolo = Convert.ToInt32(Console.ReadLine());
+
+                Rettangolo rettangolo1 = new Rettangolo(baseRettangolo, altezzaRettangolo, nomeRettangolo);
+
+                rettangolo1.stampaRettangolo();
+
+            }
+        
+        
+        
+        }
+
+
+
+
+   
+    
+    
+    
+    
+    }
+
+
+}
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+
+        public string nomeRettangolo;
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        public Rettangolo(int baser, int altezza, string nome) {
+
+            nomeRettangolo = nome;
+            baseRettangolo = baser;
+            altezzaRettangolo= altezza;
+
+        }
+        public int calcoloArea()
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        public int calcoloPerimetro()
+        {
+            return (baseRettangolo * 2) + (altezzaRettangolo * 2);
+        }
+
+        public void stampaRettangolo() {
+            Console.WriteLine("-----" + this.nomeRettangolo + "-----");
+            Console.WriteLine("\nbase " + this.baseRettangolo + " cm");
+            Console.WriteLine("\naltezza " + this.altezzaRettangolo + " cm");
+            Console.WriteLine("\nperimetro " + this.calcoloPerimetro() + " cm");
+            Console.WriteLine("\narea " + this.calcoloArea() + " m^2");
+            Console.WriteLine("-------------");
+
+
+
+        }
+      
+
+    }
+   
+
+
+
+
+}
